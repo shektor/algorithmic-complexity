c79219f9d951964d078dee080f352e5f6fdcd4c6
shektor <s.hektor@gmail.com>
2019-07-02 23:31:50 +0100

    Add durationBetween function to Benchmark class
    To calcuate the time taken for a function to complete, the start time
    will need to be subtracted from the end time of the run.

--- spec/benchmark.test.js
+++ spec/benchmark.test.js
@@ -11,3 +11,14 @@ describe('#timeNow', () => {
     expect(benchmark.timeNow()).toEqual(new Date('2019-07-02T20:03:50.895Z'))
   })
 })
+
+describe('#durationBetween', () => {
+  test('it returns time in milliseconds between two date objects', () => {
+    const startTime = new Date('2019-07-02T22:20:34.920Z')
+    const endTime = new Date('2019-07-02T22:20:42.533Z')
+
+    const benchmark = new Benchmark()
+
+    expect(benchmark.durationBetween(startTime, endTime)).toBe(7613)
+  })
+})
--- src/benchmark.js
+++ src/benchmark.js
@@ -2,6 +2,10 @@ class Benchmark {
   timeNow () {
     return new Date(Date.now())
   }
+
+  durationBetween (startTime, endTime) {
+    return endTime - startTime
+  }
 }
 
 module.exports = {
